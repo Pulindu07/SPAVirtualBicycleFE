0fab91d2f2f6954508c1f8f92203a1179db581fc
pulindu <pulindujanith@gmail.com>
2025-11-01 13:08:03 +0530

    fix endpoint

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import type { RoutePoint, UserProgress } from "../types";
 
-const API_BASE_URL = "http://localhost:5288/api";
+const API_BASE_URL =
+  "https://virtualexerciseappbe-cvecdnfsfta0axd5.eastus2-01.azurewebsites.net/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
